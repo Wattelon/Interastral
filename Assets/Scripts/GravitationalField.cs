bb4f3feba2838873bd32e79ae1b7c2af5da30c16
Wattelon <Mr.Smayl546@gmail.com>
2023-12-01 23:24:35 +0300

    Visuals are finally nice, time to code

---
+++ Assets/Scripts/GravitationalField.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitationalField : MonoBehaviour
+{
+    [SerializeField] private float density;
+    private float _mass;
+    private float _radius;
+
+    private void Start()
+    {
+        _radius = transform.parent.lossyScale.x;
+        _mass = Mathf.Pow(_radius, 3) * density;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.TryGetComponent(out Rigidbody collision))
+        {
+            Debug.Log(other.name);
+            var pullDirection = transform.position - collision.transform.position;
+            var sqrDistance = pullDirection.sqrMagnitude;
+            pullDirection.Normalize();
+            var force = _mass / sqrDistance;
+            collision.AddForce(pullDirection * force, ForceMode.Force);
+        }
+    }
+}
