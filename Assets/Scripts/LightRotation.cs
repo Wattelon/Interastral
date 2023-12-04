1a01edd43bb0b096d3b2fa56ace8a4274ebbe7d0
Wattelon <Mr.Smayl546@gmail.com>
2023-12-04 15:27:53 +0300

    Interior collision fix

---
+++ Assets/Scripts/LightRotation.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotation : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed;
+
+    private Transform _transform;
+    
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    private void Update()
+    {
+        _transform.Rotate(Vector3.up * rotationSpeed);
+    }
+}
